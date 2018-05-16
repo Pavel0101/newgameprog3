2503a539437274d4eb3fdccb5f5f35c576a8d2c3
Your Name <you@example.com>
2018-05-16 19:14:27 +0400

    eff

--- newgameprog3/script.js
+++ newgameprog3/script.js
@@ -1,79 +1,86 @@
 function genMatrix(w, h) {
     var matrix = [];
-    for(var y = 0; y < h; y++) {
+    for (var y = 0; y < h; y++) {
         matrix[y] = [];
-        for(var x = 0; x < w; x++) {
+        for (var x = 0; x < w; x++) {
             var r = random(100);
-            if     (r < 20) r = 0;
-            else if(r < 65) r = 1;
-            else if(r < 90) r = 2;
-            else if(r < 100)r = 3;
+            if (r < 2) r = 0;
+            else if (r < 4) r = 1;
+            else if (r < 90) r = 2;
+            else if (r < 100) r = 3;
             matrix[y][x] = r;
         }
     }
     return matrix;
 }
-var imgold;
+class player {
+    constructor(x, y, nkar) {
+        this.x = x;
+        this.y = y;
+        this.nkar = nkar;
+    }
+}
 var matrix;
 var w = 32;
 var h = 32;
 var side = 32;
 
+
 function preload() {
     img = loadImage("gold.png");
+    img1 = loadImage("obstacle_1.png");
+    blue = new player(2, 990, loadImage("player_blue_2.png"));
 }
 
 function setup() {
-    matrix = genMatrix(w,h);
+    matrix = genMatrix(w, h);
     createCanvas(side * w, side * h);
-     console.log(matrix);
+    console.log(matrix);
+    frameRate(60);
+}
+function draw() {
     background("green");
-    frameRate(5);
-}
-function draw() {
-    for(var y in matrix) {
-        for(var x in matrix[y]) {
-            if(matrix[y][x] == 0) {
-                 image(img, x * side, y * side)
+    for (var y in matrix) {
+        for (var x in matrix[y]) {
+            if (matrix[y][x] == 0) {
+                image(img, x * side, y * side);
             }
         }
     }
-}
-/*
-function draw() {
-    for(var y in matrix) {
-        for(var x in matrix[y]) {
-            if(matrix[y][x] == 0) {
-                fill("#acacac");
+    for (var y in matrix) {
+        for (var x in matrix[y]) {
+            if (matrix[y][x] == 1) {
+                image(img1, x * side, y * side);
             }
-            else if(matrix[y][x] == 1) {
-                fill("green");
-            }
-            else if(matrix[y][x] == 2) {
-                fill("yellow");
-            }
-            else if(matrix[y][x] == 3) {
-                fill("red");
-            }
-            rect(x * side, y * side, side, side);
         }
     }
-
-    for(var i in grassArr) {
-        grassArr[i].mul();
+    image(blue.nkar, blue.x, blue.y);
+        if (keyIsDown(UP_ARROW)) {
+        blue.y = blue.y - side / 8;
+    } else if (keyIsDown(DOWN_ARROW)) {
+        blue.y = blue.y + side / 8;
     }
-
-    for(var i in xotakerArr) {
-        xotakerArr[i].bazmanal();
-        xotakerArr[i].utel();
-        xotakerArr[i].mahanal();
+    if (keyIsDown(LEFT_ARROW)) {
+        blue.x = blue.x - side / 8;
+    } else if (keyIsDown(RIGHT_ARROW)) {
+        blue.x = blue.x + side / 8;
     }
+}
 
-    for(var i in gishatichArr) {
-        gishatichArr[i].bazmanal();
-        gishatichArr[i].utel();
-        gishatichArr[i].mahanal();
-    }
+
+var playerX, playerY => fixvac
+for(var i in obstacles) {
+    var objectX = obstacles[i].x;
+
+    if(collision) return;
+}
+
+for(var i in gold) {
+    var objectX = obstacles[i].x;
+
+    if(collision) nkary hanel u vercnel voski;
+}
+
+for(var i in energy) {
 
 }
-*/
